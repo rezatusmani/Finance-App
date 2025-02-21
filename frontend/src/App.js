8d7ba896ca1339e780cbf207c253d7647bf1fc73
Reza Usmani <rezatusmani@gmail.com>
2025-02-21 13:07:55 -0600

    removed plaid from app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import ExpenseTracker from './ExpenseTracker'; // Import ExpenseTracker component
-import PlaidLinkButton from './PlaidLinkButton'; // Import new PlaidLink component (we'll create this)
 
 function App() {
     return (
@@ -14,15 +13,11 @@ function App() {
                         <li>
                             <Link to="/expense-tracker" className="nav-link">Expense Tracker</Link>
                         </li>
-                        <li>
-                            <Link to="/plaid-link" className="nav-link">Connect Bank Account</Link>
-                        </li>
                     </ul>
                 </nav>
 
                 <Routes className="app-routes">
                     <Route path="/expense-tracker" element={<ExpenseTracker />} />
-                    <Route path="/plaid-link" element={<PlaidLinkButton />} /> {/* Add Plaid Link route */}
                     <Route path="/" element={<h2 className="welcome-message">Welcome to My App! Select a feature from the menu.</h2>} />
                 </Routes>
             </div>
